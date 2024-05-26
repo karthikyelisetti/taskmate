a86ba4b0f89c1b6f6f07b95b17ce517cfb9173e8
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2024-05-26 10:27:22 +0530

    Added the Tasks list code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Header } from './components/Header';
+import { AddTask } from './components/AddTask';
+import { ShowTask } from './components/ShowTask';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Header />
+      <AddTask />
+      <ShowTask />
     </div>
   );
 }
